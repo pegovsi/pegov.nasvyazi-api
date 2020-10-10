4d1991ba03d9cb04c6a356e41886df6a135e957d
pegov <sergei.pegov@stecpoint.ru>
2020-10-10 16:14:06 +0300

    init

--- Pegov.Nasvyazi.Application/Buisness/Organization/Commands/CreateOrganization/CreateOrganizationCommandHandler.cs
+++ Pegov.Nasvyazi.Application/Buisness/Organization/Commands/CreateOrganization/CreateOrganizationCommandHandler.cs
@@ -1,11 +1,23 @@
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Pegov.Nasvayzi.Application.Infrastructure;
 using Pegov.Nasvayzi.Common;
+using Pegov.Nasvyazi.Application;
 
 namespace Pegov.Nasvayzi.Application.Buisness.Organization.Commands.CreateOrganization
 {
     public class CreateOrganizationCommandHandler: HandlerBase<CreateOrganizationCommand, Result<Guid>>
     {
-        
+        public CreateOrganizationCommandHandler(IRavenStore store, ICurrentUserService currentUserService) 
+            : base(store, currentUserService)
+        {
+        }
+
+        public override async Task<Result<Guid>> Handle(CreateOrganizationCommand request, CancellationToken cancellationToken)
+        {
+            return ResultHelper.Success(Guid.NewGuid());
+
+        }
     }
 }
